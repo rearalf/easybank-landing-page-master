5cd4e00fb5485b18d076907aca4781e42411d00f
Ricardo Alf <rear_recinos@hotmail.com>
2024-01-19 09:05:45 -0600

    feat: add navbar component

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,13 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+	integrations: [tailwind()],
+	head: {
+		links: [
+			{
+				rel: 'stylesheet',
+				href: 'https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;700&display=swap',
+			},
+		],
+	},
+})
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				//* Primary
+				'dark-blue': 'hsl(233, 26%, 24%)',
+				'lime-green': 'hsl(136, 65%, 51%)',
+				'bright-cyan': 'hsl(192, 70%, 51%)',
+				//* Neutral
+				'grayish-blue': 'hsl(233, 8%, 62%)',
+				'light-grayish-blue': 'hsl(220, 16%, 96%)',
+				'very-light-gray': 'hsl(0, 0%, 98%)',
+			},
+			fontFamily: {
+				customFont: ['Public Sans', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
